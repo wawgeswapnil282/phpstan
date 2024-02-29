7abdb6bb10befb41cd31c7088e7fd6b9b49db2b3
wawgeswapnil282 <110432471+wawgeswapnil282@users.noreply.github.com>
2024-02-29 13:08:16 -0700

    Update test.php

--- test.php
+++ test.php
@@ -1,1 +1,9 @@
-echo 'Test';
+<?php declare(strict_types = 1);
+
+class HelloWorld
+{
+	public function sayHello(DateTimeImutable $date): void
+	{
+		echo 'Hello, ' . $date->format('j. n. Y');
+	}
+}
